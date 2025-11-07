63e989732310cf236d32a001c72e42556ad5e56e
areej-png <areejamir890@gmail.com>
2025-11-07 17:20:49 +0500

    feat: setup main App component and import Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero"
+
 export default function App() {
-  return <h1>Portfolio Coming Soon...</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
