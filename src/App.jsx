1ac603562319a0d9cdc4d1ba179c3582247e63a9
areej-png <areejamir890@gmail.com>
2025-11-10 10:38:08 +0500

    Update App.jsx to include Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Project from "./components/Project";
-import Contact from "./components/Contact"
-
+import Contact from "./components/Contact";
+import Footer from "./components/Footer"
 
 export default function App() {
   return (
@@ -15,6 +15,7 @@ export default function App() {
       <Skills /> 
       <Project />
       <Contact />
+      <Footer />   
     </>
   );
 }
