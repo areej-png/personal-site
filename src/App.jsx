1e54e73d722ffe02aebef0f8e78f5edd5fae5cbb
areej-png <areejamir890@gmail.com>
2025-11-12 20:42:31 +0500

    feat: integrate React Router and add dedicated Resume page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Resume from "./components/Resum"; 
 import Project from "./components/Project";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Hero />
-      <About /> 
-      <Skills /> 
-      <Project />
-      <Contact />
-      <Footer />   
-    </>
+      <Routes>
+        {/* Portfolio main page */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <About />
+              <Skills />
+              <Project />
+              <Contact />
+              <Footer />
+            </>
+          }
+        />
+
+        {/* Resume page */}
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+    </Router>
   );
 }
